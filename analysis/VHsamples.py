f99a0449465f6b68bcfffdb04b28f06ee2d23b44
Tyler <tyler.lam@physics.ucla.edu>
2024-02-16 16:18:10 -0600

    Cleaning up samples definitions

--- analysis/VHsamples.py
+++ analysis/VHsamples.py
@@ -11,46 +11,4 @@ for s in samples_{s}_{era}:
         samples[s] = samples_{s}_{era}[s]
 """.format(s=s, era=era)
 
-        #cmd = "import analysis.samples.VHsamples_{s}_{era} as samples_{s}_{era}".format(s=s, era=era)
         exec(cmd)
-
-#import pdb; pdb.set_trace()
-'''
-samples={}
-
-for s in samples_data_2018:
-    if s not in samples:
-        samples[s] = samples_data_2018[s]
-
-for s in samples_signal_2018:
-    if s not in samples:
-        samples[s] = samples_signal_2018[s]
-
-for s in samples_mc_2018:
-    if s not in samples:
-        samples[s] = samples_mc_2018[s]
-
-for s in samples_data_2017:
-    if s not in samples:
-        samples[s] = samples_data_2017[s]
-
-for s in samples_mc_2017:
-    if s not in samples:
-        samples[s] = samples_mc_2017[s]
-
-for s in samples_sig_2017:
-    if s not in samples:
-        samples[s] = samples_signal_2017[s]
-
-for s in samples_data_2016:
-    if s not in samples:
-        samples[s] = samples_data_2016[s]
-
-for s in samples_mc_2016:
-    if s not in samples:
-        samples[s] = samples_mc_2016[s]
-
-for s in samples_mc_2016:
-    if s not in samples:
-        samples[s] = samples_mc_2016[s]
-'''
