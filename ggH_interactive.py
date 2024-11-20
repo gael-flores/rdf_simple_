ea16594493068220360a4000085bde812363039b
Michalis Bachtis <bachtis@cmsinter01.physics.ucla.edu>
2024-11-20 15:34:52 -0800

    Adding gGH interactive"

--- ggH_interactive.py
+++ ggH_interactive.py
@@ -3,23 +3,23 @@ from common.plotter import *
 luminosity=87.09
 
 #make a plotter for the antiID data that will be used to estimate the background
-background = rdf_plotter('data_ggH4g_antiID.root',False,'0.0009943081222885356','ggH4g_antiID')
+#background = rdf_plotter('data_ggH4g_antiID.root',False,'0.0009943081222885356','ggH4g_antiID')
 #make a plotter for the signal
-signal = rdf_plotter('ggH4g.root',True,'48780*0.01')
+signal = rdf_plotter('ggH_M30_ctau0_ggH4g.root',True,'1','ggH4g','1','Report_ggH4g')
 #make a plotter for the blinded data 
-data = rdf_plotter('data_ggH4g.root',False,'1','ggH4g')
+#data = rdf_plotter('data_ggH4g.root',False,'1','ggH4g')
 
 #set the colors
-background.setFillProperties(1001,ROOT.kAzure-9)
+#background.setFillProperties(1001,ROOT.kAzure-9)
 signal.setFillProperties(0,ROOT.kWhite)
 signal.setLineProperties(1,ROOT.kRed,3)
 
 
 #create the stack and add the plotters above
 stack = combined_plotter()
-stack.add_plotter(background,"bkg","background",typeP = "background")
-stack.add_plotter(signal,"signal","ggH #rightarrow #phi#phi#rightarrow 4#gamma (BR=1%)",typeP = "signal")
-stack.add_plotter(data,"data","data",typeP = "data")
+#stack.add_plotter(background,"bkg","background",typeP = "background")
+stack.add_plotter(signal,"signal","ggH #rightarrow #phi#phi#rightarrow 4#gamma ",typeP = "signal")
+#stack.add_plotter(data,"data","data",typeP = "data")
 
 
 
