5ac86a658d3f32430ddc006de5d427ee3ae15058
Benjamin Jobilal <bjobilal@g.ucla.edu>
2025-12-16 02:33:55 +0100

    Completed 2023, 2024 data and MC samples. Recomputed some cross-sections for Run 3 2022 MC samples

--- analysis/samples/VHsamples_mc_2022.py
+++ analysis/samples/VHsamples_mc_2022.py
@@ -9,7 +9,7 @@ samples_mc_2022 = {
                          'veto_triggers': [],
                          'era': '2022',
                          'jobs': 8,
-                         'sigma': 5558.0},
+                         'sigma': 5731.0},
 
 
 'DYJetsToLL_M50_LO_2022': {'dataset': '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Summer22NanoAODv11-forPOG_126X_mcRun3_2022_realistic_v2-v1/NANOAODSIM',
@@ -27,10 +27,77 @@ samples_mc_2022 = {
                        'jobs': 8,
                        'sigma': 56120.},
 
-#2022 W+Gamma MC - no datasets appear on DAS for 2022
+#2022 W+Gamma MC
 
+'WGtoLNuG_1Jets_PTG100to200_EE_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':2.219}, 
 
-#2022 W+GammaGamma - no datasets appear on DAS for 2022
+'WGtoLNuG_1Jets_PTG100to200_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':2.221}, 
+
+'WGtoLNuG_1Jets_PTG10to100_EE_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-10to100_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':662.2}, 
+
+'WGtoLNuG_1Jets_PTG10to100_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-10to100_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v2/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':662.2},
+
+'WGtoLNuG_1Jets_PTG200to400_EE_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-200to400_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v3/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.2908}, 
+
+'WGtoLNuG_1Jets_PTG200to400_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-200to400_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v1/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.2908}, 
+
+'WGtoLNuG_1Jets_PTG400to600_EE_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-400to600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v3/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.02231},
+
+'WGtoLNuG_1Jets_PTG400to600_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-400to600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v1/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.02231},
+
+'WGtoLNuG_1Jets_PTG600_EE_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v3/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.004907}, 
+
+'WGtoLNuG_1Jets_PTG600_2022': {'dataset':'/WGtoLNuG-1Jets_PTG-600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v3/NANOAODSIM',
+                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                           'veto_triggers':[],
+                           'era':'2022',
+                           'jobs':1,
+                           'sigma':0.004907},
 
 
 #2022 tt+Gamma
@@ -47,7 +114,7 @@ samples_mc_2022 = {
                            'veto_triggers':[],
                            'era':'2022',
                            'jobs':1,
-                           'sigma':0.4114},
+                           'sigma':0.4072},
 
 'TTGJets_1Jets_PTG-10to100_2022': {'dataset':'/TTG-1Jets_PTG-10to100_TuneCP5_13p6TeV_amcatnloFXFXold-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v1/NANOAODSIM',
                            'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
@@ -61,7 +128,7 @@ samples_mc_2022 = {
                            'veto_triggers':[],
                            'era':'2022',
                            'jobs':1,
-                           'sigma':4.216},
+                           'sigma':4.237},
 
 
 #2022 tt+GammaGamma
@@ -77,9 +144,6 @@ samples_mc_2022 = {
                            'veto_triggers':[],
                            'era':'2022',
                            'jobs':1,
-                           'sigma':0.02391},
-
-
-#2022 tt+Jets MC - no datasets appear on DAS for 2022
+                           'sigma':0.02391}
 
 }
--- analysis/samples/VHsamples_mc_2024.py
+++ analysis/samples/VHsamples_mc_2024.py
@@ -3,83 +3,56 @@
 #source for calculating cross sections using GenXSecAnalyzer: https://twiki.cern.ch/twiki/bin/view/CMSPublic/GeneratorsHATSatLPC2019InDevelopment#Using_GenXSecAnalyzer
 
 samples_mc_2024 = {
-# 2022 DY+Jets MC
-'DYJetsToLL_M50_LO_EE_2022': {'dataset': '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Summer22EENanoAODv11-forPOG_126X_mcRun3_2022_realistic_postEE_v1-v1/NANOAODSIM',
+#2024 DY+Jets MC
+'DYJetsToLL_M50_NoPU_v6_v2_2024': {'dataset': '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter24NanoAOD-NoPU_Pilot_133X_mcRun3_2024_realistic_v6-v2/NANOAODSIM',
                          'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
                          'veto_triggers': [],
-                         'era': '2022',
+                         'era': '2024',
                          'jobs': 8,
-                         'sigma': 5558.0},
+                         'sigma': 5669.0}, 
 
-
-'DYJetsToLL_M50_LO_2022': {'dataset': '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Summer22NanoAODv11-forPOG_126X_mcRun3_2022_realistic_v2-v1/NANOAODSIM',
+'DYJetsToLL_M50_v26-v2_2024': {'dataset': '/DYJetsToLL_M-50_TuneCP5_13p6TeV-madgraphMLM-pythia8/RunIII2024Summer24NanoAOD-Pilot2024wmLHEGS_140X_mcRun3_2024_realistic_v26-v2/NANOAODSIM',
                          'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
                          'veto_triggers': [],
-                         'era': '2022',
+                         'era': '2024',
                          'jobs': 8,
-                         'sigma': 5558.0},
+                         'sigma': 5634.0}, 
 
-# 2022 W+Jets MC
-'WJetsToLNu_LO_2022': {'dataset': '/WJetsToLNu_TuneCP5_13p6TeV-madgraphMLM-pythia8/Run3Winter22NanoAOD-122X_mcRun3_2021_realistic_v9-v1/NANOAODSIM',
+#2024 W+Gamma MC
+'WGtoLNuG_1Jets_PTG-100_2024': {'dataset': '/WGtoLNuG-1Jets_Bin-PTG-100_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM',
                        'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
                        'veto_triggers': [],
-                       'era': '2022',
+                       'era': '2024',
                        'jobs': 8,
-                       'sigma': 56120.},
+                       'sigma': 2.551},
 
-#2022 W+Gamma MC - no datasets appear on DAS for 2022
+'WGtoLNuG_1Jets_PTG-200_2024': {'dataset': '/WGtoLNuG-1Jets_Bin-PTG-200_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM',
+                       'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                       'veto_triggers': [],
+                       'era': '2024',
+                       'jobs': 8,
+                       'sigma': 0.3176}, 
 
+'WGtoLNuG_1Jets_PTG-400_2024': {'dataset': '/WGtoLNuG-1Jets_Bin-PTG-400_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM',
+                       'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                       'veto_triggers': [],
+                       'era': '2024',
+                       'jobs': 8,
+                       'sigma': 0.02642}, 
 
-#2022 W+GammaGamma - no datasets appear on DAS for 2022
+'WGtoLNuG_1Jets_PTG-600_2024': {'dataset': '/WGtoLNuG-1Jets_Bin-PTG-600_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM',
+                       'triggers': ['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
+                       'veto_triggers': [],
+                       'era': '2024',
+                       'jobs': 8,
+                       'sigma': 0.004754}, 
 
-
-#2022 tt+Gamma
-
-'TTGJets_1Jets_PTG-100to200_2022': {'dataset':'/TTG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFXold-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v3/NANOAODSIM',
+#2024 tt+GammaGamma
+'TTGG_LO_2024': {'dataset': '/TTGG_TuneCP5_13p6TeV_madgraph-madspin-pythia8/RunIII2024Summer24NanoAODv15-150X_mcRun3_2024_realistic_v2-v2/NANOAODSIM',
                            'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
                            'veto_triggers':[],
-                           'era':'2022',
+                           'era':'2024',
                            'jobs':1,
-                           'sigma':0.4114},
-
-'TTGJets_1Jets_PTG-100to200_EE_2022': {'dataset':'/TTG-1Jets_PTG-100to200_TuneCP5_13p6TeV_amcatnloFXFXold-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v3/NANOAODSIM',
-                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
-                           'veto_triggers':[],
-                           'era':'2022',
-                           'jobs':1,
-                           'sigma':0.4114},
-
-'TTGJets_1Jets_PTG-10to100_2022': {'dataset':'/TTG-1Jets_PTG-10to100_TuneCP5_13p6TeV_amcatnloFXFXold-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v1/NANOAODSIM',
-                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
-                           'veto_triggers':[],
-                           'era':'2022',
-                           'jobs':1,
-                           'sigma':4.216},
-
-'TTGJets_1Jets_PTG-10to100_EE_2022': {'dataset':'/TTG-1Jets_PTG-10to100_TuneCP5_13p6TeV_amcatnloFXFXold-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v4/NANOAODSIM',
-                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
-                           'veto_triggers':[],
-                           'era':'2022',
-                           'jobs':1,
-                           'sigma':4.216},
-
-
-#2022 tt+GammaGamma
-'TTGG_LO_2022': {'dataset': '/TTGG_TuneCP5_13p6TeV_madgraph-madspin-pythia8/Run3Summer22NanoAODv11-126X_mcRun3_2022_realistic_v2-v2/NANOAODSIM',
-                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
-                           'veto_triggers':[],
-                           'era':'2022',
-                           'jobs':1,
-                           'sigma':0.02391},
-
-'TTGG_LO_EE_2022': {'dataset': '/TTGG_TuneCP5_13p6TeV_madgraph-madspin-pythia8/Run3Summer22EENanoAODv12-130X_mcRun3_2022_realistic_postEE_v6-v2/NANOAODSIM',
-                           'triggers':['(HLT_IsoMu24||HLT_Ele30_WPTight_Gsf)'],
-                           'veto_triggers':[],
-                           'era':'2022',
-                           'jobs':1,
-                           'sigma':0.02391},
-
-
-#2022 tt+Jets MC - no datasets appear on DAS for 2022
+                           'sigma':0.02391}
 
 }
